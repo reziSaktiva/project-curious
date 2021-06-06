ad0755aa5f4e40a20be8890f066b8f2a985a679d
Personal Github <maximillianonico8@gmail.com>
2021-06-06 20:21:29 +0700

    create query search algolia

--- backend/functions/graphql/resolvers/posts.js
+++ backend/functions/graphql/resolvers/posts.js
@@ -5,6 +5,11 @@ const { AuthenticationError, UserInputError } = require('apollo-server-express')
 const fbAuthContext = require("../../utility/fbAuthContext");
 const randomGenerator = require("../../utility/randomGenerator");
 
+const ALGOLIA_ID = 'EIAYYZTM49';
+const ALGOLIA_ADMIN_KEY = '2a9c7905cd20fc8e32c70646ce563def';
+const algoliasearch = require('algoliasearch');
+const client = algoliasearch(ALGOLIA_ID, ALGOLIA_ADMIN_KEY);
+
 module.exports = {
   Query: {
     async getPosts(_, { lat, lng, range }) {
@@ -63,7 +68,9 @@ module.exports = {
 
             const distance = computeDistanceBetween(currentLatLng, contentLocation)
 
-            if ((distance / 1000) <= 40) { // should be show in range 40 km
+            console.log('id post: ', data.id,' distance: ', distance);
+
+            if ((distance / (range || 1000)) <= 40) { // should be show in range 40 km
               nearby.push(newData);
             }
           } catch (e) {
@@ -302,6 +309,66 @@ module.exports = {
       } catch (error) {
         console.log(error);
       }
+    },
+    async setRulesSearchAlgolia(_, { rank, index }, ctx) {
+      if (!index) {
+        throw new UserInputError('index search is required, check index name on algolia dashboard')
+      }
+      
+      if (!rang || rank.length) {
+        throw new UserInputError('rank is Required')
+      }
+
+      const algoIndex = client.initIndex(index);
+
+      return new Promise((resolve, reject) => {
+        algoIndex.setSettings({ customRanking: rank })
+          .then(() => {
+            resolve("Success");
+          })
+          .catch(() => {
+            reject("Failed set rules")
+          })
+      })
+    },
+    async textSearch(_, { search, perPage = 5, page, range = 40, location }, context) {
+      const { lat, lng } = location;
+      const index = client.initIndex('search_posts');
+
+      const defaultPayload = {
+        "attributesToRetrieve": "*",
+        "attributesToSnippet": "*:20",
+        "snippetEllipsisText": "…",
+        "responseFields": "*",
+        "getRankingInfo": true,
+        "analytics": false,
+        "enableABTest": false,
+        "explain": "*",
+        "facets": ["*"],
+        "customRanking": ['desc(likeCount)', 'desc(commentCount)', "words"] // ranking based on likeCount and commentCount
+      };
+      const geoLocPayload = {
+        "aroundLatLng": `${lat}, ${lng}`,
+        "aroundRadius": range * 1000,
+      };
+
+      const pagination = {
+        "hitsPerPage": perPage || 10,
+        "page": page || 0,
+      }
+
+      return new Promise((resolve, reject) => {
+        index.search(search, { ...defaultPayload, ...geoLocPayload, ...pagination})
+          .then(res => {
+            const { hits, page, nbHits, nbPages, hitsPerPage, processingTimeMS } = res;
+            
+            // return following structure data algolia
+            resolve({ hits, page, nbHits, nbPages, hitsPerPage, processingTimeMS })
+          }).catch(err => {
+            reject(err)
+            console.error(err)
+          });
+      });
     },
     async getPost(_, { id }, context) {
       const { username } = await fbAuthContext(context)
@@ -593,9 +660,16 @@ module.exports = {
       return [];
     },
     async createPost(_, { text, media, location, repost }, context) {
+
       const { username } = await fbAuthContext(context);
       if (username) {
         try {
+          const regexpHastag = /(\s|^)\#\w\w+\b/gm
+          let hastags = text.match(regexpHastag) || [];
+          if (hastags) {
+            hastags = hastags.map((s) => s.trim());
+          }
+
           const newPost = {
             owner: username,
             text,
@@ -603,7 +677,8 @@ module.exports = {
             createdAt: new Date().toISOString(),
             likeCount: 0,
             commentCount: 0,
-            location
+            location,
+            _tags: hastags
           };
 
           if (repost) {
@@ -615,6 +690,20 @@ module.exports = {
             .add(newPost)
             .then((doc) => {
               newPost.id = doc.id;
+              const index = client.initIndex('search_posts');
+              index.saveObjects([
+                {
+                  ...newPost,
+                  objectID: doc.id,
+                  _geoloc: location
+                }], { autoGenerateObjectIDIfNotExist: false })
+                .then(({ objectIDs }) => {
+                  console.log(objectIDs);
+                })
+                .catch(err => {
+                  console.log(err);
+                });
+
               doc.update({ id: doc.id });
             });
 
